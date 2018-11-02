aa627803ca0dcd80743fe1c0777e111a91bff6b7
Michael Paskett - Laptop <michael.paskett@utah.edu>
2018-11-02 07:23:56 -0600

    Skeleton for forceDirectedNetwork

---
+++ Source Code/js/script.js
@@ -0,0 +1,12 @@
+// initiate necessary objects
+
+let forceDirectedNetwork = new forceDirectedNetwork();
+
+let yearSlider = new yearSlider();
+
+let horizontalBars = new horizontalBars();
+
+let impactTrace = new impactTrace();
+
+let infoBox = new infoBox();
+
