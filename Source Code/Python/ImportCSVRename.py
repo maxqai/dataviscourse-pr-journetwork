4424da1ad8a2a6e7551ed3adba763266762ca8fd
BPhilip7 <u1138890@utah.edu>
2018-11-07 16:46:56 -0700

    Made the CSV Preprocesser. Steps: 1. Download The Files. 2. That's It.

---
+++ Source Code/Python/ImportCSVRename.py
@@ -0,0 +1,26 @@
+import csv
+from os import listdir
+from os.path import isfile, join
+import re
+
+
+path = 'C:/Users/bphil/Downloads'
+
+
+csvs = []
+l = listdir(path)
+for f in l:
+    if f.find('.csv') != -1:
+        if f == 'JournalProfileGrid.csv':
+            JName = f
+            with open(path + '/' + JName) as f1:
+                for idx, line in enumerate(f1):
+                    if idx == 0:
+                        word = line.split(',')
+                        if word[0].find('Journal Profile: ') == 0:
+                            des_word = word[0][17::]
+            csvs.append(f)
+        elif f == "JournalCitingTab.csv" or f == "JournalCitedTab.csv":
+            csvs.append(f)
+print(csvs)
+print(des_word)
