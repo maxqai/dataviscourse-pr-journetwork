a9e26830cff5e595e3fb79710af73b319dd6bda9
Michael Paskett - Lab <michael.paskett@utah.edu>
2018-11-07 14:48:39 -0700

    Started python script for preprocessing

---
+++ Source Code/Python/dataPreprocessing.py
@@ -0,0 +1,12 @@
+from os import listdir
+
+# get names of csv's categorized by type
+allCSVs = listdir('data')
+print(allCSVs)
+
+# https://docs.python.org/3/howto/regex.html
+journalCitedList = []
+for i in range(len(allCSVs)):
+    if allCSVs[i].find('JournalCitedTab'):
+        # journalCitedList.push(allCSVs[i])
+
--- Source Code/js/script.js
+++ Source Code/js/script.js
@@ -11,10 +11,10 @@ let impactTrace = new ImpactTrace();
 let journalInfoBox = new JournalInfoBox();
 
 // load all data
-// const fs = require('fs');
-// const dir = './data';
-//
-// fs.readdir(dir, (err, files) => {
-//   console.log(files.length);
-// });
+const fs = require('fs');
+const dir = './data';
+
+fs.readdir(dir, (err, files) => {
+  console.log(files.length);
+});
 // create first network, maybe first in alphabet? That with largest impact factor?
