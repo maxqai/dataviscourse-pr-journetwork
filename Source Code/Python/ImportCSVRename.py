110cfce0e30716734fac6f22e190fdab02a07175
BPhilip7 <u1138890@utah.edu>
2018-11-09 11:21:09 -0700

    This will load data, rename it, and organize it into a JSON. Must store data in data presort

--- Source Code/Python/ImportCSVRename.py
+++ Source Code/Python/ImportCSVRename.py
@@ -2,11 +2,14 @@ import csv
 from os import listdir
 from os.path import isfile, join
 import re
+import json
 
 
 path = 'Source Code/data_presort'
+path2 = 'Source Code/data'
+# Make sure wd = dataviscourse-pr-journetwork
 
-
+# Finds the Names of the Files and Renames them Appropriately
 csvs = []
 l = listdir(path)
 for f in l:
@@ -22,5 +25,25 @@ for f in l:
             csvs.append(f)
         elif f == "JournalCitingTab.csv" or f == "JournalCitedTab.csv":
             csvs.append(f)
-print(csvs)
-print(des_word)
+
+NewFileNames = []
+[NewFileNames.append(path2 + '/' + des_word + f) for f in csvs]
+# print(NewFileNames)
+
+
+# Finds the csv file and turn to json file
+l = listdir(path)
+for f in l:
+    if f.find('.csv') != -1:
+        print(f)
+        if f == 'JournalProfileGrid.csv':
+            JName = f
+            csvfile = open(path + '/' + f,'r')
+            jsonfile = open(NewFileNames[2],'w')
+            print(jsonfile)
+            fieldnames = ('Year','Total Cites', 'Journal Impact Factor', 'Impact Factor without Journal Self Cites', '5-Year Impact Factor'	'Immediacy Index',	'Citable Items', 'Cited Half-Life',	'Citing Half-life',	'Eigenfactor Score', 'Article Influence Score',	'% Articles in Citable Items', 'Normalized Eigenfactor', 'avgJifPercentile')
+            reader = csv.DictReader(csvfile, fieldnames)
+            for row in reader:
+                json.dump(row, jsonfile)
+                jsonfile.write('\n')
+
