b1fc30f2a8acbe548661bc65ed3e13f0f5754841
Michael Paskett - Lab <michael.paskett@utah.edu>
2018-11-09 10:38:42 -0700

    Working on forceDirectedNetwork

--- Source Code/js/forceDirectedNetwork.js
+++ Source Code/js/forceDirectedNetwork.js
@@ -2,7 +2,22 @@
 // adapted from votePercentageChart.js
 class ForceDirectedNetwork {
 
-    constructor(){
+    constructor(yearSlider, horizontalBars, impactTrace, journalInfoBox){
+        // Make accessible
+		this.yearSlider = yearSlider;
+		this.horizontalBars = horizontalBars;
+		this.impactTrace = impactTrace;
+		this.journalInfoBox = journalInfoBox;
+		// Data - thinking I have to do this in the update since it changes
+        // this.profileGrid = journalCSVs[0];
+        // this.citedTab = journalCSVs[1];
+        // this.citingTab = journalCSVs[2];
+        // this.year = initialYear;
+        // console.log('prfGrid', this.profileGrid);
+        // console.log('citedTab', this.citedTab);
+        // console.log('citingTab', this.citingTab);
+        // console.log('initYear', this.year);
+
         //initialize svg elements, svg sizing
         this.margin = {top: 10, right: 50, bottom: 20, left: 50};
         let divForceDirectedNetwork = d3.select("#network").classed("network", true);
@@ -16,6 +31,7 @@ class ForceDirectedNetwork {
         this.svg = divForceDirectedNetwork.append("svg")
             .attr("width", this.svgWidth)
             .attr("height", 30); //TODO: fix this to not be hardcoded
+
 
 
 
@@ -43,12 +59,27 @@ class ForceDirectedNetwork {
 	    return text;
 	}
 
-	/**
-	 * Creates the forceDirectedNetwork, content and tool tips
-	 *
-	 * @param selectedJournal - Journal selection
-	 */
-	update (selectedJournal){
+    /**
+     * Creates the forceDirectedNetwork, content and tool tips
+     *
+     * @param selectedJournal - Journal selection
+     * @param year - year for display
+     */
+	update (journalCSVs, year){
+        this.profileGrid = journalCSVs[0];
+        this.citedTab = journalCSVs[1];
+        this.citingTab = journalCSVs[2];
+        this.year = year;
+        // console.log('prfGrid', this.profileGrid);
+        // console.log('citedTab', this.citedTab);
+        // console.log('citingTab', this.citingTab);
+        // console.log('Year', this.year);
+
+	    // make scale for circle sizes (have to sqrt for area)
+        let impactFactorScale = d3.scaleLinear()
+            .domain([0,1])
+            .range([0,1])
+
 
 			// this.tip.html((d)=> {
 			// 		let tooltip_data = {
@@ -62,6 +93,28 @@ class ForceDirectedNetwork {
 			// bars
 			// 	.on('mouseover', this.tip.show)
 			// 	.on('mouseout', this.tip.hide);
+        //TODO: When adding interactivity, use something like below
+        // circles
+        //     .attr("cx", d => yearScale(d.YEAR))
+        //     .attr("cy", this.svgHeight/3)
+        //     .attr("r", 10)
+        //     .attr("class", d => this.chooseClass(d.PARTY))
+        //     .on('mouseover', function() {
+        //         d3.select(this).classed('highlighted', true)
+        //     })
+        //     .on('mouseout', function() {
+        //         d3.select(this).classed('highlighted', false)
+        //     })
+        //     .on('click', d =>  {
+        //         d3.select("#year-chart").selectAll("circle").classed('selected', false);
+        //         d3.select("#year-chart").selectAll("circle").filter(selYear => selYear.YEAR === d.YEAR)
+        //             .classed('selected', true);
+        //         d3.csv("data/Year_Timeline_" + d.YEAR + ".csv").then(selectedYear => {
+        //             this.electoralVoteChart.update(selectedYear, this.colorScale);
+        //             this.tileChart.update(selectedYear, this.colorScale);
+        //             this.votePercentageChart.update(selectedYear);
+        //         })
+        //     });
 	};
 
 
--- Source Code/js/script.js
+++ Source Code/js/script.js
@@ -1,6 +1,6 @@
 // initiate necessary objects
 
-let forceDirectedNetwork = new ForceDirectedNetwork();
+// let forceDirectedNetwork = new ForceDirectedNetwork();
 
 let yearSlider = new YearSlider();
 
@@ -12,18 +12,32 @@ let journalInfoBox = new JournalInfoBox();
 
 // load all data
 // begin with loading a single one... Nature!
-// let profGrid = d3.csv("data/NATUREJournalProfileGrid.csv"); // all d3 commands look at first line only for header... looks like no way around modifying the csv
+// let profGrid = d3.csvParseRows("data/NATUREJournalProfileGrid.csv"); // all d3 commands look at first line only for header... looks like no way around modifying the csv
 // console.log('profGrid', profGrid);
-// d3.csvParse("data/NATUREJournalProfileGrid.csv").then(data => {
-//     console.log(data);
-// });
-d3.csv("data/NATUREJournalProfileGrid.csv").then(profGrid => {
-    console.log('profGrid', profGrid);
+let initialYear = 2017;
+let journalFiles = ["data/NATUREJournalProfileGrid.csv", "data/NATUREJournalCitedTab.csv", "data/NATUREJournalCitingTab.csv"];
+let promises = [];
+journalFiles.forEach( file => {
+    promises.push(d3.csv(file));
 });
+
+let journalCSVs = undefined;
+Promise.all(promises).then( data => {
+    journalCSVs = data;
+    // console.log('values', journalCSVs);
+    let forceDirectedNetwork = new ForceDirectedNetwork(yearSlider, horizontalBars, impactTrace, journalInfoBox);
+    forceDirectedNetwork.update(journalCSVs, initialYear);
+});
+// d3.csv("data/NATUREJournalProfileGrid.csv").then(data => {
+//     console.log('profGrid',data);
+// });
+// d3.csv("data/NATUREJournalProfileGrid.csv").then(profGrid => {
+//     console.log('profGrid', profGrid);
+// });
 // d3.csv("data/yearwiseWinner.csv").then(electionWinners => {
 //     // console.log(electionWinners);
 //     let yearChart = new YearChart(electoralVoteChart, tileChart, votePercentageChart, electionWinners);
 //     yearChart.update();
 // });
 
-// create first network, maybe first in alphabet? That with largest impact factor?
+
